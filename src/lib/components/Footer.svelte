28c5396fef10efb5f63fae4c93695c193e4fe8ef
DanielBoye <83395536+DanielBoye@users.noreply.github.com>
2022-04-30 18:53:15 +0200

    XKR Whitepaper Temp Fix

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -12,7 +12,7 @@
             <div class="footer-nav">
                 <h3>XKR</h3>
                 <a href="https://explorer.kryptokrona.se" class="footer-link"><p>Explorer</p></a>
-                <a href="/static/Whitepaper/Proposal_for_a_New_Nordic_Digital_Currency.pdf" class="footer-link"><p>Whitepaper</p></a>
+                <a href="https://github.com/kryptokrona/xkr-website/blob/main/static/Whitepaper/Proposal_for_a_New_Nordic_Digital_Currency.pdf" class="footer-link"><p>Whitepaper</p></a>
                 <a href="/blog" class="footer-link"><p>Blog</p></a>
                 <a href="/https://docs.kryptokrona.org" class="footer-link"><p>Guides</p></a>
                 <a href="https://github.com/kryptokrona" class="footer-link"><p>Github</p></a>
