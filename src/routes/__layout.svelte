86e31a39975ab0a293c08c9d774f2af7a6b68afb
Swepool <36674091+Swepool@users.noreply.github.com>
2022-05-01 03:36:34 +0200

    popup bg and layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,13 +4,30 @@
     import LoadingScreen from "/src/lib/components/LoadingScreen.svelte";
     import Navbar from "/src/lib/components/navbar/Navbar.svelte";
     import Footer from "/src/lib/components/Footer.svelte";
+    import Popup from "$lib/components/Popup.svelte";
+
+    //Global scss
     import '$lib/theme/global.scss'
+    import {browser} from "$app/env";
 
     let ready
+    let visited
+    let showPopup = false
+
     onMount(() => {
         ready = true
+
+        //Checks if it's a new visitor, and renders popup.
+        visited = localStorage.getItem('visited')
+        if(!visited) {
+            localStorage.setItem('visited', 'true')
+            setInterval(() => {
+                showPopup = true
+            },7000)
+        }
     });
 
+    //If we're mounted we wait x (1 second) amount of time to make sure fonts etc is ready. Looks good as well.
     $: {
         if (ready) {
             setInterval(() => {
@@ -21,10 +38,17 @@
 
 </script>
 
+<!--Loading screen with animated logo-->
 {#if $state.loading}
     <LoadingScreen/>
 {/if}
 
+<!--Popup, currently saying join discord to newVisitors-->
+{#if showPopup}
+    <Popup on:click={showPopup = false}/>
+{/if}
+
+<!--Layout with navbar and footer, slot is all the content being rendered in <main>, dont add a main tag elsewhere. -->
 <Navbar/>
 <main>
     {#if ready}
@@ -34,7 +58,7 @@
 <Footer/>
 
 <style lang="scss">
-
+/* We hande the website width and padding at top level */
   main {
     margin: 0 auto;
     max-width: var(--website-width);
