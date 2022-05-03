e58d4682a63498e78d083a3097496ebd633f597f
Swepool <36674091+Swepool@users.noreply.github.com>
2022-05-03 23:36:13 +0200

    Update index.svelte

--- src/routes/mining/index.svelte
+++ src/routes/mining/index.svelte
@@ -1,15 +1,16 @@
 <script>
-import MiningConfig from "$lib/components/MiningConfig.svelte";
-import JoinDiscord from "$lib/components/JoinDiscord.svelte";
-import StatusBar from "$lib/components/StatusBar.svelte";
-import MiningCalc from "$lib/components/MiningCalc.svelte";
+    import MiningConfig from "$lib/components/MiningConfig.svelte";
+    import JoinDiscord from "$lib/components/JoinDiscord.svelte";
+    import StatusBar from "$lib/components/StatusBar.svelte";
+    import MiningCalc from "$lib/components/MiningCalc.svelte";
+    import LinkButton from "../../lib/components/buttons/LinkButton.svelte";
 </script>
 
 <div class="wrapper">
     <h1>Mining</h1>
 
     <div class="text-wrapper">
-        <p>Kryptokrona is a cryptocurrency that relies on proof-of-work mining to achieve distributed consensus. Learn more</p>
+        <p>Kryptokrona is a cryptocurrency that relies on proof-of-work mining to achieve distributed consensus.</p>
         <h2>Hardware</h2>
         <p>Kryptokrona can be mined on CPUs, GPUs & Android Phones. </p>
         <h2>Software</h2>
@@ -18,10 +19,14 @@ import MiningCalc from "$lib/components/MiningCalc.svelte";
         <p>Join our community</p>
     </div>
 
+    <LinkButton url="https://xmrig.com/download" target="_blank" enabled={true} text="Download XMRig"/>
+
     <div class="tools">
         <MiningConfig/>
         <MiningCalc/>
     </div>
+
+    <div class=""></div>
     <JoinDiscord/>
     <StatusBar/>
 </div>
@@ -34,23 +39,23 @@ import MiningCalc from "$lib/components/MiningCalc.svelte";
 
 <style lang="scss">
 
-    .wrapper {
-      margin-bottom: 40px;
-      box-sizing: border-box;
-    }
+  .wrapper {
+    margin-bottom: 40px;
+    box-sizing: border-box;
+  }
 
-    .text-wrapper {
-      margin-bottom: 40px;
-    }
+  .text-wrapper {
+    margin-bottom: 40px;
+  }
 
-    .tools {
-      display: flex;
-      gap: 40px;
-      margin-bottom: 40px;
+  .tools {
+    display: flex;
+    gap: 40px;
+    margin: 40px 0;
 
-      @media only screen and (max-width: 1000px) {
-        flex-direction: column;
-      }
+    @media only screen and (max-width: 1000px) {
+      flex-direction: column;
     }
+  }
 
 </style>
