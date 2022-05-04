a0a1801dc3d03bd277d40ba8718f603e60ec2981
Swepool <36674091+Swepool@users.noreply.github.com>
2022-05-04 18:32:22 +0200

    removed disabled

--- src/lib/components/buttons/Button.svelte
+++ src/lib/components/buttons/Button.svelte
@@ -3,10 +3,10 @@
     export let enabled = false
     export let rgb = false
     export let info = false
-    export let disabled;
+
 </script>
 
-<button disabled={disabled}
+<button
         on:click
         class:enabled
         class:info
