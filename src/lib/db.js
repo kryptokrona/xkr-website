1cf81b6c1323a469b6be227be2852aa478e23fa0
Swepool <36674091+Swepool@users.noreply.github.com>
2022-05-04 21:34:55 +0200

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,11 +4,10 @@ let supabase
 const supabaseUrl = import.meta.env.VITE_SVELTE_APP_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SVELTE_APP_SUPABASE_ANON_KEY;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'development') {
     supabase = createClient(supabaseUrl, supabaseAnonKey)
-} else if (process.env.NODE_ENV === 'development') {
+} else {
     console.log('Supabase disabled in development')
-    console.log(process.env)
 }
 
 export default supabase
