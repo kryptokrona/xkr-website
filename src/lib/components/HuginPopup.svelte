9dc0b3db430e56986aef9a5d6fd935388da440cd
Swepool <36674091+Swepool@users.noreply.github.com>
2022-05-13 00:19:28 +0200

    color

--- src/lib/components/HuginPopup.svelte
+++ src/lib/components/HuginPopup.svelte
@@ -32,7 +32,7 @@
                        stroke-linecap="round">
                         <g id="Group-23"
                            transform="translate(39.500000, 39.500000) rotate(-315.000000) translate(-39.500000, -39.500000) translate(14.500000, 14.500000)"
-                           stroke="#FFFFFF" stroke-width="5">
+                           stroke="var(--title-color)" stroke-width="5">
                             <line x1="2.36161665e-16" y1="25" x2="50" y2="25" id="Line-9"></line>
                             <line x1="25" y1="50" x2="25" y2="1.91800598e-15" id="Line-9"></line>
                         </g>
@@ -84,7 +84,7 @@
         left: 0;
         width: 1rem;
         height: 1rem;
-        background-color: var(--success-color);
+        background-color: var(--warn-color);
         border-radius: 50%;
       }
 
