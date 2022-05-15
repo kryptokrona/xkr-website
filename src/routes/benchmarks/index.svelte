74909e4e44ba641e4a4dd72e034e8dc601714e41
DanielBoye <danielboye888@gmail.com>
2022-05-15 21:55:58 +0200

    Started on a /benchmark

---
+++ src/routes/benchmarks/index.svelte
@@ -0,0 +1,49 @@
+<script>
+    import MiningConfig from "$lib/components/MiningConfig.svelte";
+    import JoinDiscord from "$lib/components/JoinDiscord.svelte";
+    import StatusBar from "$lib/components/StatusBar.svelte";
+    import MiningCalc from "$lib/components/MiningCalc.svelte";
+    import LinkButton from "../../lib/components/buttons/LinkButton.svelte";
+</script>
+
+<div class="wrapper">
+    <h1>Benchmarks</h1>
+
+    <table>
+        <thead>
+          <tr>
+            <th>Month</th>
+            <th>Savings</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>January</td>
+            <td>$100</td>
+          </tr>
+          <tr>
+            <td>February</td>
+            <td>$80</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>Sum</td>
+            <td>$180</td>
+          </tr>
+        </tfoot>
+      </table>
+</div>
+
+<style lang="scss">
+
+    .wrapper {
+      margin-top: 160px;
+      margin-bottom: 40px;
+      box-sizing: border-box;
+      color: white;
+    }
+    table, th, td {
+      border: 1px solid white;
+    }
+</style>
