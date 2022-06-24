e2e07e1660d2c4cc53f505f75d557f3ec7c87772
DanielBoye <danielboye888@gmail.com>
2022-06-25 00:08:52 +0200

    asd

---
+++ src/i18n/de/index.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @typedef { import('../i18n-types').Translation } Translation
+ */
+
+/** @type { Translation } */
+const de = {
+	// this is an example Translation, just rename or delete this folder if you want
+	test: 'blablabla',
+	HI: 'hi',
+}
+
+export default de
