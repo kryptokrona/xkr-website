b979ab2f093dfa8162f9a57ab0d30e83bc24926c
Lukas Nyberg <Lukas.nyberg@gmail.com>
2022-07-02 14:23:09 +0200

    csp mode auto

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
             fallback: 'index.html',
         }),
         csp: {
-            mode: 'nonce',
+            mode: 'auto',
             directives: {
                 "script-src": ['self'],
                 "base-uri": ['self'],
