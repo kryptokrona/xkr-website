1ec7d0038c47978fb2f012791221c00d36e9a79e
Lukas <36674091+Swepool@users.noreply.github.com>
2022-08-03 16:58:46 +0200

    edits

--- src/routes/[lang]/about/index.svelte
+++ src/routes/[lang]/about/index.svelte
@@ -40,34 +40,36 @@
     <link rel="alternate" href="https://kryptokrona.org/about" hreflang="x-default"/>
 </svelte:head>
 
-<div class="text-wrapper">
-    <h1>About</h1>
-    <h2>Money for the future</h2>
-    <p>Kryptokrona exists to secure and simplify our future economic system. Sending and receiving money should not be
-        expensive or slow. We work with open source code that allows you to be involved and improve the money of the
-        future.</p>
-    <p>Kryptokrona is a decentralized blockchain based on CryptoNote, which forms the basis for Monero, among others.
-        CryptoNote is a so-called “application layer” protocol further developed by TurtleCoin that enables things like:
-        private transactions, messages and arbitrary data storage, completely decentralized.</p>
-    <p>On 2 April 2019, the platform was launched in part as a response to the Riksbank’s digital e-krona for the
-        people’s
-        right to create money and to safeguard the private economy without either commercial or store control.</p>
-</div>
-<div class="features">
-    <div>
-       <h2>Max Supply</h2>
-        <p>1,000,000,000 XKR</p>
-        <h2>Current Supply</h2>
-        <p>{$network.supply} XKR</p>
+<div class="container">
+    <div class="text-wrapper">
+        <h1>About</h1>
+        <h2>Money for the future</h2>
+        <p>Kryptokrona exists to secure and simplify our future economic system. Sending and receiving money should not be
+            expensive or slow. We work with open source code that allows you to be involved and improve the money of the
+            future.</p>
+        <p>Kryptokrona is a decentralized blockchain based on CryptoNote, which forms the basis for Monero, among others.
+            CryptoNote is a so-called “application layer” protocol further developed by TurtleCoin that enables things like:
+            private transactions, messages and arbitrary data storage, completely decentralized.</p>
+        <p>On 2 April 2019, the platform was launched in part as a response to the Riksbank’s digital e-krona for the
+            people’s
+            right to create money and to safeguard the private economy without either commercial or store control.</p>
     </div>
-    {#each about as card}
+    <div class="features">
         <div>
-            <h2>{card.title}</h2>
-            <p>{card.text}</p>
+            <h2>Max Supply</h2>
+            <p>1,000,000,000 XKR</p>
+            <h2>Current Supply</h2>
+            <p>{$network.supply} XKR</p>
         </div>
-    {/each}
+        {#each about as card}
+            <div>
+                <h2>{card.title}</h2>
+                <p>{card.text}</p>
+            </div>
+        {/each}
+    </div>
+    <StatusBar/>
 </div>
-<StatusBar/>
 
 <style lang="scss">
 
