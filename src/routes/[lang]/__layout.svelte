6d2a1cdb8d0b0c81e7632efa66022c7b33be2442
Lukas <36674091+Swepool@users.noreply.github.com>
2022-08-03 16:58:24 +0200

    Update __layout.svelte

--- src/routes/[lang]/__layout.svelte
+++ src/routes/[lang]/__layout.svelte
@@ -5,28 +5,14 @@
     import LoadingScreen from "$lib/components/LoadingScreen.svelte";
     import Navbar from "$lib/components/navbar/Navbar.svelte";
     import Footer from "$lib/components/Footer.svelte";
-    import Popup from "$lib/components/Popup.svelte";
-    import HuginPopup from "$lib/components/HuginPopup.svelte";
 
     //Global scss
     import '$lib/theme/global.scss'
 
     let ready
-    let visited
-    let showPopup = false
 
     onMount(() => {
         ready = true
-
-        //Checks if it's a new visitor, and renders popup.
-        visited = localStorage.getItem('visited')
-
-        if (!visited) {
-            localStorage.setItem('visited', 'true')
-            setTimeout(() => {
-                showPopup = true
-            }, 4000)
-        }
     });
 
     //If we're mounted we wait x (1 second) amount of time to make sure fonts etc is ready. Looks good as well.
@@ -38,6 +24,7 @@
         }
     }
 
+    console.log($page.url)
 </script>
 
 <!--Loading screen with animated logo-->
@@ -45,18 +32,11 @@
     <LoadingScreen/>
 {/if}
 
-<!--Popup, currently saying join discord to newVisitors-->
-{#if showPopup}
-    <Popup on:click={showPopup = false}/>
-{/if}
 
 <!--Layout with navbar and footer, slot is all the content being rendered in <main>, dont add a main tag elsewhere. -->
 <Navbar/>
-<main class="container">
+<main>
     <slot/>
 </main>
 <Footer/>
-{#if ($page.url.pathname !== '/hugin')}
-    <HuginPopup/>
-{/if}
 
