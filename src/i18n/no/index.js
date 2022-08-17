e98965044e18d5ff50593ca5a1c81f4808c6d05f
DanielBoye <danielboye888@gmail.com>
2022-08-17 18:53:47 +0200

    Norwegian translation
    Norwegian translation of no/index.js so far

--- src/i18n/no/index.js
+++ src/i18n/no/index.js
@@ -1,18 +1,95 @@
 // @ts-check
-
+// Written by: https://github.com/DanielBoye
 /**
  * @typedef { import('../i18n-types').BaseTranslation } BaseTranslation
  */
 
 /** @type { BaseTranslation } */
 const no = {
-	navbar: {
-		wallet: 'Wallet',
-		hugin: 'Hugin',
-		mining: 'Mining',
-		articles: 'Articles',
-		about: 'About'
-	}
+    navbar: {
+        wallet: 'Lommebok',
+        hugin: 'Hugin',
+        mining: 'Utvinning',
+        articles: 'Artikler',
+        about: 'Om oss',
+    },
+    footer: {
+        title1: 'Navigasjon',
+        home: 'Hjem',
+        wallet: 'Lommebok',
+        mining: 'Utvinning',
+        store: 'Butikk',
+        about: 'Om oss',
+        articles: 'Artikler',
+        stickers: 'Stickers',
+        docs: 'Dokumentasjon',
+
+    },
+    components: {
+        hero: {
+          title: 'DESENTRALISERT PRIVAT KOMMUNIKASJONS- OG BETALINGSSYSTEM.',
+          text: 'En nordisk kryptovaluta for å sikre og forenkle vårt fremtidige økonomiske system. Det skal ikke være dyrt eller tregt å sende og motta penger. Vi jobber med åpen kildekode som lar deg være med og forbedre og utvikle fremtidens kommunikasjonsystem og penger.'
+        },
+        statusBar: {
+            height: 'Høyde',
+            hashrate: 'Hashrate',
+            nodes: 'Noder'
+        },
+        features: {
+            title1: 'Nordisk Krypto',
+            text1: 'En voksende kryptovaluta fra Norden, med visjonen å sikre nordisk fortreffelighet innen utvikling av sikre systemer og økonomiske verktøy.',
+            title2: 'Desentralisert',
+            text2: 'Kryptokrona gir verdi som et robust desentralisert betalings- og kommunikasjonssystem over alle landegrenser. Dette er med på å sikre vår frihet som borgere.',
+            title3: 'Krypto-fellesskap',
+            text3: 'Sterk lokal forankring med forståelse for politikk, kultur og språk. En funksjon som ikke bør undervurderes.',
+            title4: 'Et svar',
+            text4: 'Kryptokrona lansert som et åpen kildekode-alternativ til en digital e-krone/e-krona og det voksende kontantløse samfunnet i Norden.',
+            title5: 'Ingen mellommann',
+            text5: 'Kryptokrona er ikke avhengig av noen sentralbank eller server, og er dermed mindre skjøre enn dagens finansielle systemer.',
+            title6: 'Privat',
+            text6: 'Det grunnleggende i Kryptokrona lar brukere være anonyme som standard, sende verdier og data uten å bli avslørt eller sporet.',
+        },
+        community: {
+            title: 'Krypto-fellesskap',
+            text: 'Bli med i et raskt voksende fellesskap av utviklere og innovatører koblet over hele verden. Prosjektet er åpen kildekode og vedlikeholdes av fellesskapet. Du kan for eksempel gjøre endringer på denne siden, foreslå fantastiske nye funksjoner eller hjelpe oss med å squash bugs. Ikke en utvikler? Ingen bekymringer, det er tonnevis av ting til \nå gjør!',
+            huginText: 'Bli med i vår personverns første messenger.',
+            discordText: 'Tekniske spørsmål om våre verktøy? Spør en utvikler på Discord serveren vi har!',
+            telegramText: 'Still generelle spørsmål og chat med det verdensomspennende fellesskapet på Telegram.',
+            twitterText: 'Følg @kryptokrona for å få de siste nyhetene og oppdateringene fra hele økosystemet.!'
+        },
+        mining: {
+            title: 'Utvinning',
+            text: 'Gjennom det som er kjent som utvinning av kryptovaluta, ser nettverket etter Kryptokrona. Denne mekanismen sikrer nettverket. Du blir automatisk belønnet av nettverket for din datamaskins arbeid',
+            button: 'Kom i gang'
+        },
+        huginChat: {
+            title: 'Skjul deg med Hugin',
+            text: 'Hugin Messenger setter personvernet ditt først. Ved bruk av militær sikker kryptering er meldingene dine trygge for andres nysgjerrige øyne. Hugin bruker den desentraliserte strukturen som får kryptovalutaer til å fungere, noe som betyr at systemet ikke kan tas ned eller kontrolleres av en enhet.',
+            button: 'Lær mer'
+        },
+        fixedBanner: {
+            button: 'Bli med oss!'
+        }
+    },
+    about: {
+        title: 'Om oss',
+        subTitle: 'Penger for fremtiden',
+        mainText1: 'Kryptokrona er til for å sikre og forenkle vårt fremtidige økonomiske system. Det skal ikke være dyrt eller tregt å sende og motta penger. Vi jobber med åpen kildekode som lar deg være med og forbedre fremtidens penger.',
+        mainText2: 'Kryptokrona er en desentralisert blokkjede basert på CryptoNote, som danner grunnlaget for blant annet Monero. CryptoNote er en såkalt «application layer»-protokoll som ble videreutviklet av TurtleCoin som muliggjør ting som: private transaksjoner, meldinger og vilkårlig datalagring, fullstendig desentralisert.',
+        mainText3: 'På 2 April 2019, ble platformen lansert delvis som et svar på Riksbankens digitale e-krone for folkets rett til å skape penger og for å ivareta privatøkonomien uten verken kommersiell interesse eller butikkkontroll.',
+        maxSupply: 'Maks forsyning',
+        currentSupply: 'Nåværende forsyning',
+        title2: 'GPU Utvinning Algoritme',
+        text2: 'Vi kjører for tiden på CryptoNight-Pico, som er en mer rettferdig algoritme enn Bitcoins SHA256 og kan til og med utvinnes på ARM-baserte telefoner!',
+        title3: 'Blocktime',
+        text3: 'Nettverket vårt behandler nye transaksjoner hvert 90. sekund, sammenlignet med Bitcoins 10 minutters forsinkelse.',
+        title4: 'Usporbar',
+        text4: 'XKR transaksjoner bruker ringsignaturer og stealth-adresser for å holde nysgjerrige øyne unna dine private transaksjoner!',
+        title5: 'Fungible',
+        text5: 'Kryptokrona er ikke avhengig av våre sentralbanker eller servere, og er dermed mindre skjøre enn dagens finansielle systemer.',
+        title6: 'Drama-free',
+        text6: 'Nettverket vårt reduserer mengden av nye mynter preget med hver blokk, noe som gir en dramafri utslipp.',
+    }
 }
 
 export default no
